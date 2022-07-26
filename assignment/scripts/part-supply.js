ed8b3ab01070ca28ff0307f451ca60d69a0f8236
Ryan Hess <ryanmhess@gmail.com>
2022-07-26 07:50:34 -0500

    Completed numbers 1-5 of Part 3

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -12,15 +12,28 @@ console.log('Parts Needed:', partsNeeded);
 //    the following numbers: 3, 5, -6, 0, 7, 11
 console.log('\n2. Array of supplyChanges:');
 
+let supplyChanges = [3, 5, -6, 0, 7, 11];
+console.log('The supply change includes:', supplyChanges);
+
 // 3. Console log the value of the second item in the 'supplyChanges' array
 console.log('\n3. Second supplyChange is:');
+
+console.log('The second item in the supply change is:', supplyChanges[1]);
 
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
 //    array & console.log the value removed.
 console.log('\n4. Removed item:');
 
+console.log('The last item of the supply change was added by mistake.');
+let mistakeInventory = supplyChanges.pop();
+console.log(mistakeInventory, 'has been successfully removed from the supply change.');
+
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('\n5. Adding 25 to supplyChanges.');
+
+console.log('A new delivery of 25 items has added.');
+supplyChanges.push(25);
+console.log('Updated supply changes:', supplyChanges);
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
@@ -28,6 +41,7 @@ console.log('\n5. Adding 25 to supplyChanges.');
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.' 
 console.log('\n6. Showing supplyChanges...');
+
 
 
 // STRETCH GOALS
